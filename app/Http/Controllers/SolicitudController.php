0bfe7c87ec7fdfb409e2f68e0aa95fd69b64fa59
Aaron-Shrike <aaronrv138@gmail.com>
2022-02-09 17:37:28 -0500

    Controllers(solicitud, trabajador y usuario)

---
+++ app/Http/Controllers/SolicitudController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SolicitudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        //traer datos de base datos de solic reg
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        //registra la solici
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        //mostrando una solicitud segun su codigo
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TrabajadorController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Rutas API
+Route::post('/iniciar-sesion', [UsuarioController::class, 'iniciarSesion']);
+Route::post('/cerrar-sesion', [UsuarioController::class, 'cerrarSesion']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/registrar-trabajador', [TrabajadorController::class, 'store']); 
+    
+    // Route::post('/registrar-solicitud', [, 'store']); 
 });
