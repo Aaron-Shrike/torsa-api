fd86cdb0f7184abfa6c8b6fd458fb0650c769244
DaM91rp <david_rioja_paz@outlook.es>
2022-03-22 11:42:30 -0500

    Arreglando email

--- resources/views/emails/TestEmail.blade.php
+++ resources/views/emails/TestEmail.blade.php
@@ -11,10 +11,11 @@
         <div>
             <h1>BIENVENIDO A TORSA PERU</h1>
             <h2>Hola, <b>{{$name}}</b> </h2>
-            <p>Es de nuestro agrado darle la bienvenida a nuestra empresa Torsa Perú E.I.R.L con R.U.C 20539048822
-                e informarle que su cuenta ha sido activada, debe ingresar con su número de dni
-                y su contraseña es <h3><b>{{$date}}</b></h3></p>
-            <br>
+            <p>Es de nuestro agrado darle la bienvenida a nuestra empresa Torsa Perú E.I.R.L <br> 
+                con R.U.C 20539048822 e informarle que su cuenta ha sido activada, debe ingresar <br>
+                con su número de dni y su contraseña es 
+                <h3><b>{{$date}}</b></h3></p>
+            
             <p>Un saludo, el equipo de soporte de Torsa Perú E.I.R.L</p>
         </div>
         
@@ -22,7 +23,7 @@
             <h3>Contactenos:</h3>
             
             <div>
-                <link rel="icon" href="https://www.flaticon.es/icono-premium/direccion-de-casa_4064836?term=direccion&page=1&position=5&page=1&position=5&related_id=4064836&origin=search">
+                <i class="fa-solid fa-location-dot"></i>
                 <p>
                     Jr. Alfonso Ugarte N° 696(Esquina Elias Aguirre) <br>
                     Av. Independencia N° 766 - CPM. San Antonio <br>
@@ -31,27 +32,27 @@
             </div>
             
             <div>
-                <link rel="icon" href="https://www.flaticon.es/icono-gratis/facebook_145802?term=facebook&page=1&position=4&page=1&position=4&related_id=145802&origin=search">
+                <i class="fa-solid fa-f"></i>
                 <a href="https://www.facebook.com/torsaperu/?ref=page_internal">TorsaPerú</a> 
             </div>
 
             <div>
-                <link rel="icon" href="https://www.flaticon.es/icono-premium/web_3059997?term=sitio%20web&page=1&position=12&page=1&position=12&related_id=3059997&origin=search">
+                <i class="fa-solid fa-link"></i>
                 <a href="https://torsaperublog.wordpress.com/?fbclid=IwAR1oS_-By1A5qqZAu9Jb9uO5H7EZee5mKMXBtdy743ELr5c_ZwRWxsTemrg">Página Oficial</a>
             </div>
 
             <div>
-                <link rel="icon" href="https://www.flaticon.es/icono-premium/gmail_2504727?term=gmail&page=1&position=3&page=1&position=3&related_id=2504727&origin=search">
+                <i class="fa-solid fa-envelope"></i>
                 torsaperu@gmail.com
             </div>
             
             <div>
-                <link rel="icon" href="https://www.flaticon.es/icono-premium/whatsapp_3670051?term=whatsapp&page=1&position=2&page=1&position=2&related_id=3670051&origin=search">
+                <i class="fa-solid fa-mobile-screen"></i>
                 921 841 090
             </div>
             
             <div>
-                <link rel="icon" href="https://www.flaticon.es/icono-premium/telefono_2511894?term=telefono&page=1&position=60&page=1&position=60&related_id=2511894&origin=search">
+                <i class="fa-solid fa-phone"></i>
                 (074) 235195
             </div>
             
